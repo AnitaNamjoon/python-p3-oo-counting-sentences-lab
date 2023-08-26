0f25980447526919d907808f134401f003f998be
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-26 12:22:19 +0300

    Update count_sentences.py

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value):
+        if not isinstance(value, str):
+            raise TypeError("The value must be a string.")
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        sentences = re.split(r'[.!?]', self.value)
+        sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+        return len(sentences)
